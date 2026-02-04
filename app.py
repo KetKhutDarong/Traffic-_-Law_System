1d7c560901c61584ff20c4ddcd51bdeb35f5401d
KetKhutDarong <ketkhutdarong@gmail.com>
2026-02-04 15:48:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,4 +59,7 @@ def home():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        app.run(
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080))
+    )
